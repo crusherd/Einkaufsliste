57708f39aaa34d03bae5f0ae2f72b34ca038a4ba
Manuel Hieke <mahieke90@googlemail.com>
2012-07-18 14:11:16 +0200

    add welcome page for synchronisation

--- m_Eikaufsliste_RoR/app/controllers/listings_controller.rb
+++ m_Eikaufsliste_RoR/app/controllers/listings_controller.rb
@@ -38,6 +38,21 @@ class ListingsController < ApplicationController
     end
   end
   
+  def add
+    @current_list = Shoppinglist.find_by_id(session[:shoppinglist_id])
+    @listing = Listing.new(:amount => params[:amount], :article_id => params[:article_id], :shoppinglist_id => @current_list.id)
+    
+    respond_to do |format|
+      if @listing.save
+        format.html {redirect_to listings_path, notice: 'Article was successfully add to' + @current_list.name + '.' }
+        format.json { render :json => @listings }
+      else
+        format.html { render action: "index"}
+        format.json { render json: @listing.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
   def create
     @current_shoppinglist = Shoppinglist.find_by_id(session[:shoppinglist_id])
     if !@current_shoppinglist.nil?
@@ -52,21 +67,6 @@ class ListingsController < ApplicationController
         format.json { render :json => @listings}
       else
         format.html { render action: "new" }
-        format.json { render json: @listing.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-  
-  def add
-    @current_list = Shoppinglist.find_by_id(session[:shoppinglist_id])
-    @listing = Listing.new(:amount => params[:amount], :article_id => params[:article_id], :shoppinglist_id => @current_list.id)
-    
-    respond_to do |format|
-      if @listing.save
-        format.html {redirect_to listings_path, notice: 'Article was successfully add to' + @current_list.name + '.' }
-        format.json { render :json => @listings }
-      else
-        format.html { render action: "index"}
         format.json { render json: @listing.errors, status: :unprocessable_entity }
       end
     end
---
+++ m_Eikaufsliste_RoR/app/controllers/welcome_controller.rb
@@ -0,0 +1,13 @@
+class WelcomeController < ActionController::Base
+  require 'socket'
+  
+  def index
+    # turn off reverse DNS resolution temporarily
+    @local_ip = UDPSocket.open {|s| s.connect("192.0.0.1", 1); s.addr.last}
+
+    
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+end
